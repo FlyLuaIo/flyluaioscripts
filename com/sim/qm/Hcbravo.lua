4095e6ae88c3503a2fbf728907da4929dc75453a
Wei Shuai <cpuwolf@sina.com>
2026-03-17 16:25:00 +0800

    update HCBravo .json

--- com/sim/qm/Hcbravo.lua
+++ com/sim/qm/Hcbravo.lua
@@ -7,17 +7,43 @@ local Hcbravo = oop.class(com.sim.Qmdev)
 function Hcbravo:init()
     self.QmdevId = 0x32129CCC
     _G.ilua_hw_assigned_hcbravo = 0
-    -- uluaLog('Hcbravo:init'..self.QmdevId)
 end
 
 function Hcbravo:absent(FastTurnsPerSecond)
-    if not uluaFind('cpuwolf/qmdev/HCBravo/LED/AP.autopilot') then
+    if not uluaFind('cpuwolf/qmdev/HCBravo/LED/AP_autopilot') then
         return true
     end
-    _G.idr_hcbravo_hid_fastkey = uluaFind('cpuwolf/qmdev/HCBravo/fastkeypersec')
     _G.idr_hcbravo_hid_led_int = uluaFind('cpuwolf/qmdev/HCBravo/LED/int')
-    _G.idr_hcbravo_hid_led_ap_hdg = uluaFind('cpuwolf/qmdev/HCBravo/LED/AP.hdg')
-    -- uluaSet(_G.idr_hcbravo_hid_fastkey, FastTurnsPerSecond)
+    _G.idr_hcbravo_hid_led_ap_hdg = uluaFind('cpuwolf/qmdev/HCBravo/LED/AP_hdg')
+    _G.idr_hcbravo_hid_led_ap_nav = uluaFind('cpuwolf/qmdev/HCBravo/LED/AP_nav')
+    _G.idr_hcbravo_hid_led_ap_apr = uluaFind('cpuwolf/qmdev/HCBravo/LED/AP_apr')
+    _G.idr_hcbravo_hid_led_ap_rev = uluaFind('cpuwolf/qmdev/HCBravo/LED/AP_rev')
+    _G.idr_hcbravo_hid_led_ap_alt = uluaFind('cpuwolf/qmdev/HCBravo/LED/AP_alt')
+    _G.idr_hcbravo_hid_led_ap_vs = uluaFind('cpuwolf/qmdev/HCBravo/LED/AP_vs')
+    _G.idr_hcbravo_hid_led_ap_ias = uluaFind('cpuwolf/qmdev/HCBravo/LED/AP_ias')
+    _G.idr_hcbravo_hid_led_ap_autopilot = uluaFind('cpuwolf/qmdev/HCBravo/LED/AP_autopilot')
+    _G.idr_hcbravo_hid_led_gear_leftgreen = uluaFind('cpuwolf/qmdev/HCBravo/LED/Gear_LeftGreen')
+    _G.idr_hcbravo_hid_led_gear_leftred = uluaFind('cpuwolf/qmdev/HCBravo/LED/Gear_LeftRed')
+    _G.idr_hcbravo_hid_led_gear_centergreen = uluaFind('cpuwolf/qmdev/HCBravo/LED/Gear_CenterGreen')
+    _G.idr_hcbravo_hid_led_gear_centerred = uluaFind('cpuwolf/qmdev/HCBravo/LED/Gear_CenterRed')
+    _G.idr_hcbravo_hid_led_gear_rightgreen = uluaFind('cpuwolf/qmdev/HCBravo/LED/Gear_RightGreen')
+    _G.idr_hcbravo_hid_led_gear_rightred = uluaFind('cpuwolf/qmdev/HCBravo/LED/Gear_RightRed')
+    _G.idr_hcbravo_hid_led_light_masterwarn = uluaFind('cpuwolf/qmdev/HCBravo/LED/Light_MasterWarn')
+    _G.idr_hcbravo_hid_led_light_enginefire = uluaFind('cpuwolf/qmdev/HCBravo/LED/Light_EngineFire')
+    _G.idr_hcbravo_hid_led_light_lowoil = uluaFind('cpuwolf/qmdev/HCBravo/LED/Light_LowOil')
+    _G.idr_hcbravo_hid_led_light_lowfuel = uluaFind('cpuwolf/qmdev/HCBravo/LED/Light_LowFuel')
+    _G.idr_hcbravo_hid_led_light_antiice = uluaFind('cpuwolf/qmdev/HCBravo/LED/Light_Antiice')
+    _G.idr_hcbravo_hid_led_light_starter = uluaFind('cpuwolf/qmdev/HCBravo/LED/Light_Starter')
+    _G.idr_hcbravo_hid_led_light_apu = uluaFind('cpuwolf/qmdev/HCBravo/LED/Light_APU')
+    _G.idr_hcbravo_hid_led_light_mastercaution = uluaFind('cpuwolf/qmdev/HCBravo/LED/Light_MasterCaution')
+    _G.idr_hcbravo_hid_led_light_vacuum = uluaFind('cpuwolf/qmdev/HCBravo/LED/Light_Vacuum')
+    _G.idr_hcbravo_hid_led_light_lowhydpressure = uluaFind('cpuwolf/qmdev/HCBravo/LED/Light_LowHydPressURE')
+    _G.idr_hcbravo_hid_led_lights_auxfuelpump = uluaFind('cpuwolf/qmdev/HCBravo/LED/Lights_AuxFuelPump')
+    _G.idr_hcbravo_hid_led_lights_parkingbrake = uluaFind('cpuwolf/qmdev/HCBravo/LED/Lights_ParkingBrake')
+    _G.idr_hcbravo_hid_led_lights_lowvolts = uluaFind('cpuwolf/qmdev/HCBravo/LED/Lights_LowVolts')
+    _G.idr_hcbravo_hid_led_lights_door = uluaFind('cpuwolf/qmdev/HCBravo/LED/Lights_door')
+    _G.idr_hcbravo_hid_invalid = uluaFind('cpuwolf/qmdev/HCBravo/invalid')
+    _G.idr_hcbravo_hid_fastkeypersec = uluaFind('cpuwolf/qmdev/HCBravo/fastkeypersec')
     return false
 end
 
@@ -40,6 +66,16 @@ end
 
 function Hcbravo:SetHdg(valbase, val)
     self:SetBit(0, idr_hcbravo_hid_led_ap_hdg, valbase, val)
+end
+
+-- ========
+-- Leds Nav
+function Hcbravo:GetNav(dpath)
+    self:GetBit(1, dpath)
+end
+
+function Hcbravo:SetNav(valbase, val)
+    self:SetBit(1, idr_hcbravo_hid_led_ap_nav, valbase, val)
 end
 
 return Hcbravo
--- msfs/z_HCBRAVO_GA.lua
+++ msfs/z_HCBRAVO_GA.lua
@@ -14,12 +14,12 @@ end
 uluaLog("HCBravo for GA")
 
 -- Input Key binding
-hcbravo:CfgCmd(0, "sim/autopilot/heading")
+hcbravo:CfgCmd(0, "(>K:AP_PANEL_HEADING_HOLD)")
 
 
 -- Output Led lights
-hcbravo:GetHdg("sim/cockpit2/autopilot/heading_status")
-hcbravo:GetNav("sim/cockpit2/autopilot/nav_status")
+hcbravo:GetHdg("(A:AUTOPILOT HEADING LOCK,Bool)")
+hcbravo:GetNav("(A:AUTOPILOT NAV1 LOCK,Bool)")
 
 function HCBRAVO_GA_LED_UPD()
     hcbravo:SetHdg()
