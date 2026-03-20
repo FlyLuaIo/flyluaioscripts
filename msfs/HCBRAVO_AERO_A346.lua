61f68cc891eb589bf449491c484ba94ec2a99491
Wei Shuai <cpuwolf@sina.com>
2026-03-20 20:11:52 +0800

    add more generic key binding for Hcbravo

--- msfs/HCBRAVO_AERO_A346.lua
+++ msfs/HCBRAVO_AERO_A346.lua
@@ -35,7 +35,78 @@ local pswh11 = QmdevPosSwitchInit("(L:TLS_ENG_LEVER4POS)", 0.5, "(>L:TLS_ENG_LEV
     "(>L:TLS_ENG_LEVER4POS)", 1100)
 hcbravo:CfgPSw(11, pswh11, nil, 0)
 
+-- flap down
+hcbravo:CfgRpn(14, '(>K:FLAPS_INCR)')
+-- flap up
+hcbravo:CfgRpn(15, '(>K:FLAPS_DECR)')
 
+-- trim down
+hcbravo:CfgRpn(21, '(>K:ELEV_TRIM_DN)')
+-- trim up
+hcbravo:CfgRpn(22, '(>K:ELEV_TRIM_UP)')
+
+-- gear up, this config will not hard bind landing gear handler, you still press keyboard G to play with gear
+hcbravo:CfgRpn(30, '(>K:GEAR_UP)', '(>K:GEAR_DOWN)')
+-- gear down
+-- hcbravo:CfgRpn(31, 'sim/flight_controls/landing_gear_down', 'sim/none/none')
+
+if g_hcbravo_enable_vr_functions == 1 then
+    -- Big Switch 1 top
+    hcbravo:CfgRpn(33, 'sim/VR/general/reset_view')
+    -- Big Switch 1 bottom
+    --hcbravo:CfgRpn(34, 'sim/VR/general/reset_view')
+
+    -- Big Switch 2 top
+    hcbravo:CfgRpn(35, 'sim/VR/toggle_vr')
+    -- Big Switch 2 bottom
+    --hcbravo:CfgRpn(36, 'sim/VR/toggle_vr')
+
+    -- Big Switch 1 top
+    hcbravo:CfgRpn(33, 'sim/VR/general/reset_view')
+    -- Big Switch 1 bottom
+    --hcbravo:CfgRpn(34, 'sim/VR/general/reset_view')
+
+    -- Big Switch 7 top
+    hcbravo:CfgRpn(45, 'sim/VR/toggle_3d_mouse_cursor')
+    -- Big Switch 7 bottom
+    --hcbravo:CfgRpn(46, 'sim/VR/toggle_3d_mouse_cursor')
+else
+    -- Big Switch 7 top
+    hcbravo:CfgRpn(45, '1 (>K:PARKING_BRAKE_SET)', '0 (>K:PARKING_BRAKE_SET)')
+end
+
+-- 13:DEC 12:INC
+function hcbravo_mode_cfg_ias()
+    hcbravo:CfgRpn(13, '(>K:AP_SPD_VAR_DEC)')
+    hcbravo:CfgRpn(12, '(>K:AP_SPD_VAR_INC)')
+end
+
+function hcbravo_mode_cfg_crs()
+    hcbravo:CfgRpn(13, '(>K:VOR1_OBI_DEC)')
+    hcbravo:CfgRpn(12, '(>K:VOR1_OBI_INC)')
+end
+
+function hcbravo_mode_cfg_hdg()
+    hcbravo:CfgRpn(13, '(>K:HEADING_BUG_DEC)')
+    hcbravo:CfgRpn(12, '(>K:HEADING_BUG_INC)')
+end
+
+function hcbravo_mode_cfg_vs()
+    hcbravo:CfgRpn(13, '(>K:AP_VS_VAR_DEC)')
+    hcbravo:CfgRpn(12, '(>K:AP_VS_VAR_INC)')
+end
+
+function hcbravo_mode_cfg_alt()
+    hcbravo:CfgRpn(13, '(>K:AP_ALT_VAR_DEC)')
+    hcbravo:CfgRpn(12, '(>K:AP_ALT_VAR_INC)')
+end
+
+-- 20:ALT 19:VS 18:HDG 17:CRS 16:IAS
+hcbravo:CfgFc(16, 'hcbravo_mode_cfg_ias()')
+hcbravo:CfgFc(17, 'hcbravo_mode_cfg_crs()')
+hcbravo:CfgFc(18, 'hcbravo_mode_cfg_hdg()')
+hcbravo:CfgFc(19, 'hcbravo_mode_cfg_vs()')
+hcbravo:CfgFc(20, 'hcbravo_mode_cfg_alt()')
 -- Output Led lights
 hcbravo:GetHdg('(A:AUTOPILOT HEADING LOCK,Bool)')
 hcbravo:GetNav('(A:AUTOPILOT NAV1 LOCK,Bool)')
@@ -51,14 +122,14 @@ hcbravo:GetCentergreen('(L:AB_MPL_GEAR_DOWN_LIGHT_C) (L:AB_MPL_GEAR_DOWN_LIGHT_C
 hcbravo:GetCenterred('(L:AB_MPL_GEAR_UNLOCK_LIGHT_C) (L:AB_MPL_GEAR_UNLOCK_LIGHT_C2) or')
 hcbravo:GetRightgreen('(L:AB_MPL_GEAR_DOWN_LIGHT_R)')
 hcbravo:GetRightred('(L:AB_MPL_GEAR_UNLOCK_LIGHT_R)')
-hcbravo:GetMasterwarn('(A:MASTER WARNING ACTIVE,bool)')
+hcbravo:GetMasterwarn('(L:AB_MPL_Master_Warning_Light)')
 hcbravo:GetEnginefire('')
 hcbravo:GetLowoil('(A:WARNING OIL PRESSURE, Bool)')
 hcbravo:GetLowfuel('(A:GENERAL ENG FUEL PRESSURE:index,Psi)')
 hcbravo:GetAntiice('(A:ENG ANTI ICE:index,Bool)')
 hcbravo:GetStarter('')
 hcbravo:GetApu('')
-hcbravo:GetMastercaution('(A:MASTER CAUTION ACTIVE,bool)')
+hcbravo:GetMastercaution('(L:AB_MPL_Master_Caution_Light)')
 hcbravo:GetVacuum('')
 hcbravo:GetLowhydpressure('')
 hcbravo:GetAuxfuelpump('')
