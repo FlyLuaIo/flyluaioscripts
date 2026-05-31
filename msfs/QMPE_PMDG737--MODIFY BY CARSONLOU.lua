fcd15232af38095a4f5a6ff6825fe06522857eec
Wei Shuai <cpuwolf@sina.com>
2026-06-01 06:59:19 +0800

    QMPE for PMDG737

--- msfs/QMPE_PMDG737--MODIFY BY CARSONLOU.lua
+++ msfs/QMPE_PMDG737--MODIFY BY CARSONLOU.lua
@@ -19,67 +19,7 @@ qmpe:AddTogMenu("RMP2 as NAV", "RMP2用于NAV", "g_qmpe_pmdg737_use_nav")
 
 -- ===========================================================
 -- button binding
--- TCAS STBY
-qmpe:CfgRpn(42,
-    "0 (L:switch_800_73X) - 10 div s0 :1 l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
 
--- TCAS ALT RPIG OFF
-qmpe:CfgRpn(43,
-    "10 (L:switch_800_73X) - 10 div s0 :1l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
-
--- TCAS XPNDR
-qmpe:CfgRpn(40,
-    "20 (L:switch_800_73X) - 10 div s0 :1 l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
-qmpe:CfgRpn(82,
-    "20 (L:switch_800_73X) - 10 div s0 :1 l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
-
--- TCAS TA ONLY
-qmpe:CfgRpn(41,
-    "30 (L:switch_800_73X) - 10 div s0 :1 l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
-
--- TCAS TA/RA
-qmpe:CfgRpn(81,
-    "40 (L:switch_800_73X) - 10 div s0 :1 l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
-
--- OVHD FLT ALT
-qmpe:CfgRpn(20, "21808 (>K:ROTOR_BRAKE)")
-qmpe:CfgRpn(21, "21807 (>K:ROTOR_BRAKE)")
-
--- OVHD LDG ALT
-qmpe:CfgRpn(22, "22008 (>K:ROTOR_BRAKE)")
-qmpe:CfgRpn(23, "22007 (>K:ROTOR_BRAKE)")
-
--- WX+T
-qmpe:CfgRpn(14, "91601 (>K:ROTOR_BRAKE) , 92001 (>K:ROTOR_BRAKE)")
-qmpe:CfgRpn(8, "92308 (>K:ROTOR_BRAKE) , 92208 (>K:ROTOR_BRAKE)")
-qmpe:CfgRpn(9, "92307 (>K:ROTOR_BRAKE) , 92207 (>K:ROTOR_BRAKE)")
-
--- CLOCK
-qmpe:CfgRpn(65, "31401 (>K:ROTOR_BRAKE) , 52301 (>K:ROTOR_BRAKE)")
-
--- MASTER CAUTION
-qmpe:CfgRpn(44, "34801 (>K:ROTOR_BRAKE)")
-
--- FIRE WARN BELL CUTOUT
-qmpe:CfgRpn(79, "34701 (>K:ROTOR_BRAKE)")
-
--- FLOOD BRIGHT
-if MSFS_VERSION == 0 then
-    qmpe:CfgRpn(46, "75608 (>K:ROTOR_BRAKE) , 33808 (>K:ROTOR_BRAKE) , 33708 (>K:ROTOR_BRAKE) , 9408 (>K:ROTOR_BRAKE)")
-    qmpe:CfgRpn(47, "75607 (>K:ROTOR_BRAKE) , 33807 (>K:ROTOR_BRAKE) , 33707 (>K:ROTOR_BRAKE) , 9407 (>K:ROTOR_BRAKE)")
-else
-    if g_qmpe_pmdg737_use_nav ~= 0 then
-        qmpe:CfgRpn(46,
-            "(L:OH_CB_PANEL_LIGHT_CONTROL, number) 20 - 0 max (>L:OH_CB_PANEL_LIGHT_CONTROL, number) (L:CA_BACKGROUND_BRT_CONTROL, number) 20 - 0 max (>L:CA_BACKGROUND_BRT_CONTROL, number) (L:CA_AFDS_FLOOD_LIGHT_CONTROL, number) 20 - 0 max (>L:CA_AFDS_FLOOD_LIGHT_CONTROL, number) (L:PED_FLOOD_LIGHT_CONTROL, number) 20 - 0 max (>L:PED_FLOOD_LIGHT_CONTROL, number)")
-        qmpe:CfgRpn(47,
-            "(L:OH_CB_PANEL_LIGHT_CONTROL, number) 3 + 300 min (>L:OH_CB_PANEL_LIGHT_CON, number) (L:CA_BACKGROUND_BRT_CONTROL, number) 3 + 300 min (>L:CA_BACKGROUND_BRT_CONTROL, number) (L:CA_AFDS_FLOOD_LIGHT_CONTROL, number) 3 + 300 min (>L:CA_AFDS_FLOOD_LIGHT_CONTROL, number) (L:PED_FLOOD_LIGHT_CONTROL, number) 3 + 300 min (>L:PED_FLOOD_LIGHT_CONTROL, number)")
-    else
-        qmpe:CfgRpn(46,
-            "(L:PED_PANEL_LIGHT_CONTROL, number) 6 - 0 max (>L:PED_PANEL_LIGHT_CONTROL, number) (L:CA_MAIN_PANEL_LIGHT_CONTROL, number) 6 - 0 max (>L:CA_MAIN_PANEL_LIGHT_CONTROL, number)")
-        qmpe:CfgRpn(47,
-            "(L:PED_PANEL_LIGHT_CONTROL, number) 6 + 300 min (>L:PED_PANEL_LIGHT_CONTROL, number) (L:CA_MAIN_PANEL_LIGHT_CONTROL, number) 6 + 300 min (>L:CA_MAIN_PANEL_LIGHT_CONTROL, number)")
-    end
-end
 -- RMP
 
 -- Power On/Off
@@ -139,6 +79,10 @@ qmpe:CfgRpn(12, "(L:S_ASP_INT_REC_LATCH) ! (>L:S_ASP_INT_REC_LATCH)")
 qmpe:CfgRpn(13, "(L:S_ASP_CAB_REC_LATCH) ! (>L:S_ASP_CAB_REC_LATCH)")
 -- PA RX
 -- qmpe:CfgRpn(14, "(L:S_ASP_PA_REC_LATCH) ! (>L:S_ASP_PA_REC_LATCH)")
+-- WX+T
+qmpe:CfgRpn(14, "91601 (>K:ROTOR_BRAKE) , 92001 (>K:ROTOR_BRAKE)")
+
+
 -- VHF1 TX
 -- qmpe:CfgRpn(15, "(>K:COM1_TRANSMIT_SELECT)")
 qmpe:CfgRpn(15, "73401 (>K:ROTOR_BRAKE) , 73404 (>K:ROTOR_BRAKE)")
@@ -161,9 +105,17 @@ qmpe:CfgRpn(19, "(L:XMLVAR_COM_1_Volume_VHF_C) 0.01 + 1 min (>L:XMLVAR_COM_1_Vol
 -- INT RX volume
 -- qmpe:CfgRpn(20, "(L:A_ASP_INT_VOLUME) 0.05 - 0 max (>L:A_ASP_INT_VOLUME)")
 -- qmpe:CfgRpn(21, "(L:A_ASP_INT_VOLUME) 0.05 + 1 min (>L:A_ASP_INT_VOLUME)")
+-- OVHD FLT ALT
+qmpe:CfgRpn(20, "21808 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(21, "21807 (>K:ROTOR_BRAKE)")
 -- CAB RX volume
 -- qmpe:CfgRpn(22, "(L:A_ASP_CAB_VOLUME) 0.05 - 0 max (>L:A_ASP_CAB_VOLUME)")
 -- qmpe:CfgRpn(23, "(L:A_ASP_CAB_VOLUME) 0.05 + 1 min (>L:A_ASP_CAB_VOLUME)")
+qmpe:CfgRpn(8, "92308 (>K:ROTOR_BRAKE) , 92208 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(9, "92307 (>K:ROTOR_BRAKE) , 92207 (>K:ROTOR_BRAKE)")
+-- OVHD LDG ALT
+qmpe:CfgRpn(22, "22008 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(23, "22007 (>K:ROTOR_BRAKE)")
 -- PA volume
 -- qmpe:CfgRpn(8, "(L:A_ASP_PA_VOLUME) 0.05 - 0 max (>L:A_ASP_PA_VOLUME)")
 -- qmpe:CfgRpn(9, "(L:A_ASP_PA_VOLUME) 0.05 + 1 min (>L:A_ASP_PA_VOLUME)")
@@ -198,24 +150,56 @@ qmpe:CfgRpn(39, "1 (>L:A32NX_SWITCH_RADAR_PWS_POSITION)")
 -- qmpe:CfgRpn(40, "0 (>L:A32NX_SWITCH_TCAS_POSITION)")
 -- qmpe:CfgRpn(41, "1 (>L:A32NX_SWITCH_TCAS_POSITION)")
 -- qmpe:CfgRpn(81, "2 (>L:A32NX_SWITCH_TCAS_POSITION)")
-
+qmpe:CfgRpn(40,
+    "20 (L:switch_800_73X) - 10 div s0 :1 l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
+-- TCAS TA ONLY
+qmpe:CfgRpn(41,
+    "30 (L:switch_800_73X) - 10 div s0 :1 l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
+-- TCAS TA/RA
+qmpe:CfgRpn(81,
+    "40 (L:switch_800_73X) - 10 div s0 :1 l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
 -- XPDR STBY/AUTO/ON
 -- qmpe:CfgRpn(42, "1 (>A:TRANSPONDER STATE:1, Enum)")
 -- qmpe:CfgRpn(43, "3 (>A:TRANSPONDER STATE:1, Enum)")
 -- qmpe:CfgRpn(82, "4 (>A:TRANSPONDER STATE:1, Enum)")
+-- TCAS STBY
+qmpe:CfgRpn(42,
+    "0 (L:switch_800_73X) - 10 div s0 :1 l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
+-- TCAS ALT RPIG OFF
+qmpe:CfgRpn(43,
+    "10 (L:switch_800_73X) - 10 div s0 :1l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
+-- TCAS XPNDR
+qmpe:CfgRpn(82,
+    "20 (L:switch_800_73X) - 10 div s0 :1 l0 0 > if{ 80007 (>K:ROTOR_BRAKE) l0 -- s0 g1 } l0 0 < if{ 80008 (>K:ROTOR_BRAKE) l0 ++ s0 g1 }")
+
 
 -- CAUT
--- qmpe:CfgRpn(44, "1 (>L:PUSH_AUTOPILOT_MASTERCAUT_L)", "0 (>L:PUSH_AUTOPILOT_MASTERCAUT_L)")
--- WARN
--- qmpe:CfgRpn(79, "1 (>L:PUSH_AUTOPILOT_MASTERAWARN_L)", "0 (>L:PUSH_AUTOPILOT_MASTERAWARN_L)")
+-- MASTER CAUTION
+qmpe:CfgRpn(44, "34801 (>K:ROTOR_BRAKE)")
+-- WARN: FIRE WARN BELL CUTOUT
+qmpe:CfgRpn(79, "34701 (>K:ROTOR_BRAKE)")
 
 -- INTEG LT Push
 qmpe:CfgRpn(45, "1 (>L:A32NX_DCDU_ATC_MSG_ACK)")
 
 -- INTEG LT
--- qmpe:CfgRpn(46, "(A:LIGHT POTENTIOMETER:85, Percent) 1 - 0 max 85 (>K:2:LIGHT_POTENTIOMETER_SET)")
--- qmpe:CfgRpn(47, "(A:LIGHT POTENTIOMETER:85, Percent) 1 + 100 min 85 (>K:2:LIGHT_POTENTIOMETER_SET)")
-
+-- FLOOD BRIGHT
+if MSFS_VERSION == 0 then
+    qmpe:CfgRpn(46, "75608 (>K:ROTOR_BRAKE) , 33808 (>K:ROTOR_BRAKE) , 33708 (>K:ROTOR_BRAKE) , 9408 (>K:ROTOR_BRAKE)")
+    qmpe:CfgRpn(47, "75607 (>K:ROTOR_BRAKE) , 33807 (>K:ROTOR_BRAKE) , 33707 (>K:ROTOR_BRAKE) , 9407 (>K:ROTOR_BRAKE)")
+else
+    if g_qmpe_pmdg737_use_nav ~= 0 then
+        qmpe:CfgRpn(46,
+            "(L:OH_CB_PANEL_LIGHT_CONTROL, number) 20 - 0 max (>L:OH_CB_PANEL_LIGHT_CONTROL, number) (L:CA_BACKGROUND_BRT_CONTROL, number) 20 - 0 max (>L:CA_BACKGROUND_BRT_CONTROL, number) (L:CA_AFDS_FLOOD_LIGHT_CONTROL, number) 20 - 0 max (>L:CA_AFDS_FLOOD_LIGHT_CONTROL, number) (L:PED_FLOOD_LIGHT_CONTROL, number) 20 - 0 max (>L:PED_FLOOD_LIGHT_CONTROL, number)")
+        qmpe:CfgRpn(47,
+            "(L:OH_CB_PANEL_LIGHT_CONTROL, number) 3 + 300 min (>L:OH_CB_PANEL_LIGHT_CON, number) (L:CA_BACKGROUND_BRT_CONTROL, number) 3 + 300 min (>L:CA_BACKGROUND_BRT_CONTROL, number) (L:CA_AFDS_FLOOD_LIGHT_CONTROL, number) 3 + 300 min (>L:CA_AFDS_FLOOD_LIGHT_CONTROL, number) (L:PED_FLOOD_LIGHT_CONTROL, number) 3 + 300 min (>L:PED_FLOOD_LIGHT_CONTROL, number)")
+    else
+        qmpe:CfgRpn(46,
+            "(L:PED_PANEL_LIGHT_CONTROL, number) 6 - 0 max (>L:PED_PANEL_LIGHT_CONTROL, number) (L:CA_MAIN_PANEL_LIGHT_CONTROL, number) 6 - 0 max (>L:CA_MAIN_PANEL_LIGHT_CONTROL, number)")
+        qmpe:CfgRpn(47,
+            "(L:PED_PANEL_LIGHT_CONTROL, number) 6 + 300 min (>L:PED_PANEL_LIGHT_CONTROL, number) (L:CA_MAIN_PANEL_LIGHT_CONTROL, number) 6 + 300 min (>L:CA_MAIN_PANEL_LIGHT_CONTROL, number)")
+    end
+end
 -- ECAM
 -- HUD
 qmpe:CfgRpn(78, "97901 (>K:ROTOR_BRAKE)")
@@ -269,21 +253,59 @@ qmpe:CfgRpn(64, "80601 (>K:ROTOR_BRAKE)")
 
 -- Chrone
 -- qmpe:CfgRpn(65, "0 (>H:A32NX_EFIS_L_CHRONO_PUSHED)")
+-- CLOCK
+qmpe:CfgRpn(65, "31401 (>K:ROTOR_BRAKE) , 52301 (>K:ROTOR_BRAKE)")
 
 -- XPRD ATC Keypad
-qmpe:CfgRpn(66, "(>H:Transponder0)")
-qmpe:CfgRpn(67, "(>H:Transponder2)")
-qmpe:CfgRpn(68, "(>H:Transponder3)")
-qmpe:CfgRpn(69, "(>H:Transponder4)")
-qmpe:CfgRpn(70, "(>H:Transponder5)")
-qmpe:CfgRpn(71, "(>H:Transponder6)")
-qmpe:CfgRpn(72, "(>H:Transponder7)")
-qmpe:CfgRpn(73, "(>H:Transponder0)")
-qmpe:CfgRpn(74, "(>H:TransponderCLR)")
+qmpe:CfgRpn(66, "130101 (>K:ROTOR_BRAKE)", "130104 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(67, "130201 (>K:ROTOR_BRAKE)", "130204 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(68, "130301 (>K:ROTOR_BRAKE)", "130304 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(69, "130401 (>K:ROTOR_BRAKE)", "130404 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(70, "130501 (>K:ROTOR_BRAKE)", "130504 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(71, "130601 (>K:ROTOR_BRAKE)", "130604 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(72, "130701 (>K:ROTOR_BRAKE)", "130704 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(73, "130801 (>K:ROTOR_BRAKE)", "130804 (>K:ROTOR_BRAKE)")
+qmpe:CfgRpn(74, "130901 (>K:ROTOR_BRAKE)", "130904 (>K:ROTOR_BRAKE)")
 -- autobrake
-qmpe:CfgRpn(75, "33901 (>K:ROTOR_BRAKE)")
-qmpe:CfgRpn(76, "34001 (>K:ROTOR_BRAKE)")
-qmpe:CfgRpn(77, "34101 (>K:ROTOR_BRAKE)")
+
+local dr_autobrake = iDataRef:New('(L:switch_460_73X, number)')
+local pswh_autobrake = QmdevPosSwitchInit("(L:switch_460_73X, number)", 10, "46007 (>K:ROTOR_BRAKE)",
+    "46008 (>K:ROTOR_BRAKE)", 100)
+function autobrake_low()
+    if dr_autobrake:Get() == 10 then
+        QmdevPosSwitchSet(pswh_autobrake, 20)
+    else
+        QmdevPosSwitchSet(pswh_autobrake, 10)
+    end
+end
+
+function autobrake_med()
+    if dr_autobrake:Get() == 10 then
+        QmdevPosSwitchSet(pswh_autobrake, 30)
+    else
+        QmdevPosSwitchSet(pswh_autobrake, 10)
+    end
+end
+
+qmpe:CfgFc(75, 'autobrake_low()')
+qmpe:CfgFc(76, 'autobrake_med()')
+function key_max_long_func()
+    if dr_autobrake:Get() == 10 then
+        QmdevPosSwitchSet(pswh_autobrake, 0)
+    else
+        QmdevPosSwitchSet(pswh_autobrake, 10)
+    end
+end
+
+function key_max_short_func()
+    if dr_autobrake:Get() == 10 then
+        QmdevPosSwitchSet(pswh_autobrake, 70)
+    else
+        QmdevPosSwitchSet(pswh_autobrake, 10)
+    end
+end
+
+qmpe:CfgLongFc(77, 1000, key_max_long_func, key_max_short_func)
 
 ---- RMP1
 -- inner
@@ -316,8 +338,7 @@ end
 qmpe:GetXpdr("(A:TRANSPONDER CODE:1, Number)")
 -- Expert: FBW own logic
 -- @ AUTO CLR = false
--- @ TIMEOUT = 2s
-qmpe:FakeXpdrInit(false, 2)
+qmpe:FakeXpdrInit()
 local b_xpdr_act = iDataRef:New("(A:TRANSPONDER CODE:1, Number)")
 local function xpdr_update()
     if qmpe:FakeXpdrIsTimeOut() or b_xpdr_act:ChangedUpdate() then
@@ -350,29 +371,29 @@ qmpe:GetSVhf1("(L:switch_7341_73X)")
 -- VHF1 CALL LIGHT
 qmpe:GetCVhf1("(L:I_ASP_VHF_1_CALL)")
 -- VHF1 RX LIGHT
-qmpe:GetRVhf1("(A:COM RECEIVE:1, Bool)")
+qmpe:GetRVhf1("(L:switch_7391_73X)")
 -- VHF2 TX LIGHT
 qmpe:GetSVhf2("(L:switch_7351_73X)")
 -- VHF2 CALL LIGHT
 qmpe:GetCVhf2("(L:I_ASP_VHF_2_CALL)")
 -- VHF2 RX LIGHT
-qmpe:GetRVhf2("(A:COM RECEIVE:2, Bool)")
+qmpe:GetRVhf2("(L:switch_7401_73X)")
 -- MECH TX LIGHT
-qmpe:GetSMech("(L:switch_7371_73X)")
+qmpe:GetSMech("(L:switch_7361_73X)")
 -- MECH CALL LIGHT
 qmpe:GetCMech("(L:I_ASP_INT_CALL)")
 -- MECH RX LIGHT
-qmpe:GetRMech("(L:I_ASP_INT_REC)")
+qmpe:GetRMech("(L:switch_7431_73X)")
 -- ATT TX LIGHT
-qmpe:GetSAtt("(L:switch_7361_73X)")
+qmpe:GetSAtt("(L:switch_7371_73X)")
 -- ATT CALL LIGHT
 qmpe:GetCAtt("(L:I_ASP_CAB_CALL)")
 -- ATT RX LIGHT
-qmpe:GetRAtt("(L:I_ASP_CAB_REC)")
+qmpe:GetRAtt("(L:switch_7441_73X)")
 -- PX TX LIGHT
 qmpe:GetSPa("(L:switch_7381_73X)")
--- ATT RX LIGHT
-qmpe:GetRPa("(L:I_ASP_PA_REC)")
+-- PA RX LIGHT
+qmpe:GetRPa("(L:switch_7451_73X)")
 
 -- =====ECAM
 qmpe:GetEEng("(L:A32NX_ECAM_SD_CURRENT_PAGE_INDEX) 20 ==")
