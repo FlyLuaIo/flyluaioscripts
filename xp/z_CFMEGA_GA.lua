961859f27b24bf3b681b6cdd2f79a82e1c797efe
Wei Shuai <cpuwolf@sina.com>
2026-06-29 08:47:05 +0800

    MF first lua works

--- xp/z_CFMEGA_GA.lua
+++ xp/z_CFMEGA_GA.lua
@@ -17,26 +17,19 @@ uluaLog('MobiFlight CfMega for GA')
 ---- G1000 FMS inner
 -- inner
 cfmega:CfgCmd(0, "sim/radios/stby_com1_fine_down_833")
-cfmega:CfgCmd(1, "sim/radios/stby_com1_fine_up_833")
+cfmega:CfgCmd(1, "sim/radios/stby_com1_fine_down_833")
 -- outer
-cfmega:CfgCmd(2, "sim/radios/stby_com1_coarse_down")
-cfmega:CfgCmd(3, "sim/radios/stby_com1_coarse_up")
+cfmega:CfgCmd(2, "sim/radios/stby_com1_fine_up_833")
+cfmega:CfgCmd(3, "sim/radios/stby_com1_fine_up_833")
 
----- G1000 Heading
--- inner
-cfmega:CfgCmd(4, "sim/autopilot/heading_down")
-cfmega:CfgCmd(5, "sim/autopilot/heading_up")
--- outer
-cfmega:CfgCmd(6, "sim/autopilot/heading_down")
-cfmega:CfgCmd(7, "sim/autopilot/heading_up")
 
 ---- G1000 FMS outer
 -- inner
-cfmega:CfgCmd(8, "sim/GPS/g1000n3_fms_outer_down")
-cfmega:CfgCmd(9, "sim/GPS/g1000n3_fms_outer_up")
+cfmega:CfgCmd(4, "sim/radios/stby_com1_coarse_down")
+cfmega:CfgCmd(5, "sim/radios/stby_com1_coarse_down")
 -- outer
-cfmega:CfgCmd(10, "sim/GPS/g1000n3_fms_outer_down")
-cfmega:CfgCmd(11, "sim/GPS/g1000n3_fms_outer_up")
+cfmega:CfgCmd(6, "sim/radios/stby_com1_coarse_up")
+cfmega:CfgCmd(7, "sim/radios/stby_com1_coarse_up")
 
 -- G1000 FMS push (Button, bit 12)
 cfmega:CfgCmd(12, "sim/GPS/g1000n3_cursor")
@@ -47,7 +40,7 @@ cfmega:CfgCmd(13, "sim/GPS/g1000n3_ent")
 -- OUTPUT data
 
 -- ENG RPM stepper (scale may need tuning for your stepper module)
-cfmega:GetEngRpm('sim/cockpit2/engine/indicators/N1_percent[0]', 10)
+cfmega:GetEngRpm('laminar/B738/brake/brake_press', 1)
 
 GlobalFrameLoopManager:add(function()
 	cfmega:SetEngRpm()
