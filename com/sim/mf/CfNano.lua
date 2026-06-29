0722d4bc0b6a27ceb0f0fd5c4118cee08497cf74
Wei Shuai <cpuwolf@sina.com>
2026-06-29 11:04:01 +0800

    CfNano add led module

--- com/sim/mf/CfNano.lua
+++ com/sim/mf/CfNano.lua
@@ -22,7 +22,7 @@ function CfNano:absent(FastTurnsPerSecond)
 	_G.idr_cfnano_hid_invalid = uluaFind('cpuwolf/flyluaio/CfNano/invalid')
 	_G.idr_cfnano_hid_fastkeypersec = uluaFind('cpuwolf/flyluaio/CfNano/fastkeypersec')
 	uluaSet(_G.idr_cfnano_hid_fastkeypersec, FastTurnsPerSecond)
-
+	self:InitLedModule()
 	return false
 end
 
@@ -36,6 +36,48 @@ function CfNano:Init(FastTurnsPerSecond)
 	end
 	_G.ilua_hw_assigned_cfnano = 1
 	return true
+end
+
+function CfNano:InitLedModule()
+	_G.idr_cfnano_mf_segment_mask = uluaFind('cpuwolf/mf/CfNano/segment/0/mask')
+	_G.idr_cfnano_mf_segment_points = uluaFind('cpuwolf/mf/CfNano/segment/0/points')
+	_G.idr_cfnano_mf_segment_commit = uluaFind('cpuwolf/mf/CfNano/segment/0/commit')
+	_G.idr_cfnano_mf_segment_ga_eng_rpm = uluaFind('cpuwolf/mf/CfNano/segment/0/text')
+	_G.idr_cfnano_mf_segment_brightness = uluaFind('cpuwolf/mf/CfNano/segment/0/brightness')
+
+	uluaSet(_G.idr_cfnano_mf_segment_mask, 63)
+	uluaSet(_G.idr_cfnano_mf_segment_points, 8)
+	uluaSet(_G.idr_cfnano_mf_segment_brightness, 50)
+	self.segment_commit_seq = 0
+end
+
+function CfNano:CommitSegment()
+	self.segment_commit_seq = (self.segment_commit_seq or 0) + 1
+	uluaSet(_G.idr_cfnano_mf_segment_commit, self.segment_commit_seq)
+end
+
+-- ========
+-- segment GA ENG RPM
+
+function CfNano:GetGaEngRpm(dpath)
+	self.d_ga_eng_rpm = iDataRef:New(dpath)
+end
+
+function CfNano:SetGaEngRpm(val)
+	if val == nil then
+		val = self.d_ga_eng_rpm:Get()
+		if self.d_ga_eng_rpm:ChangedUpdate() then
+			uluaSet(_G.idr_cfnano_mf_segment_ga_eng_rpm, val)
+			self:CommitSegment()
+		end
+	else
+		uluaSet(_G.idr_cfnano_mf_segment_ga_eng_rpm, val)
+		self:CommitSegment()
+	end
+end
+
+function CfNano:FreshGaEngRpm()
+	self.d_ga_eng_rpm:Invalid(-1)
 end
 
 return CfNano
--- xp/z_CFNANO_GA.lua
+++ xp/z_CFNANO_GA.lua
@@ -12,3 +12,9 @@ end
 
 uluaLog('MobiFlight CfNano for GA')
 
+cfnano:GetGaEngRpm('sim/cockpit2/radios/actuators/com1_standby_frequency_hz_833')
+
+GlobalFrameLoopManager:add(function()
+	cfnano:SetGaEngRpm()
+end)
+
