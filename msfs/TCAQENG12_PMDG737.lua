a5aee70018c52a72c8a9ada0c14136afaeb8a2ad
Wei Shuai <cpuwolf@sina.com>
2026-08-10 19:30:52 +0800

    sync carson PMDG 737/777

--- msfs/TCAQENG12_PMDG737.lua
+++ msfs/TCAQENG12_PMDG737.lua
@@ -13,10 +13,9 @@ uluaLog('TCA Q-Eng 1&2 for PMDG 737')
 -- ===========================================================
 -- button binding
 
--- SET SPOILERS ARM
---tcaqeng12:CfgRpn(39, '679201 (>K:ROTOR_BRAKE)')
--- SPOILERS DOWN
---tcaqeng12:CfgRpn(38, '679101 (>K:ROTOR_BRAKE)')
+-- SET SPOILERS ARM/DOWN
+tcaqeng12:CfgRpn(24, '(L:switch_679_73X) 0 == if{ 679201 (>K:ROTOR_BRAKE) }', '679101 (>K:ROTOR_BRAKE)')
+
 ---- ENG STSRT
 -- ENG L IDEL/CUTOFF
 tcaqeng12:CfgRpn(2, '(L:switch_688_73X) 100 == if{ 68801 (>K:ROTOR_BRAKE) }',
@@ -24,3 +23,10 @@ tcaqeng12:CfgRpn(2, '(L:switch_688_73X) 100 == if{ 68801 (>K:ROTOR_BRAKE) }',
 -- ENG R IDEL/CUTOFF
 tcaqeng12:CfgRpn(3, '(L:switch_689_73X) 100 == if{ 68901 (>K:ROTOR_BRAKE) }',
     '(L:switch_689_73X) 0 == if{ 68901 (>K:ROTOR_BRAKE) }')
+
+---- AUTOBREAK
+local pswh15 = QmdevPosSwitchInit("(L:switch_460_73X, number)", 10, "46007 (>K:ROTOR_BRAKE)", "46008 (>K:ROTOR_BRAKE)",
+    100)
+tcaqeng12:CfgPSw(21, pswh15, 10, 0)
+tcaqeng12:CfgPSw(22, pswh15, 20)
+tcaqeng12:CfgPSw(23, pswh15, 30)
--- msfs/TCAQENG12_PMDG777.lua
+++ msfs/TCAQENG12_PMDG777.lua
@@ -8,7 +8,7 @@ local tcaqeng12 = com.sim.qm.Tcaqeng12.Open()
 if not tcaqeng12 then return end
 -- Do not remove above lines: hardware detection
 
-uluaLog('Tcaqeng12 for PMDG 777')
+uluaLog('TCA Q-Eng 1&2 for PMDG 777')
 
 -- ===========================================================
 -- button binding
@@ -24,6 +24,7 @@ tcaqeng12:CfgRpn(2, '(L:switch_520_a) 100 == if{ 52001 (>K:ROTOR_BRAKE) }',
 -- ENG R IDEL/CUTOFF
 tcaqeng12:CfgRpn(3, '(L:switch_521_a) 100 == if{ 52101 (>K:ROTOR_BRAKE) }',
     '(L:switch_521_a) 0 == if{ 52101 (>K:ROTOR_BRAKE) }')
+
 ---- AUTOBREAK
 local pswh15 = QmdevPosSwitchInit("(L:switch_292_a, number)", 10, "29207 (>K:ROTOR_BRAKE)", "29208 (>K:ROTOR_BRAKE)", 100)
 tcaqeng12:CfgPSw(21, pswh15, 10, 0)
