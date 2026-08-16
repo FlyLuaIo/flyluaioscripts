c83b7316f6ae9ae2d145d3934dae3d55d2523642
Wei Shuai <cpuwolf@sina.com>
2026-08-16 21:58:03 +0800

    ursa vib

--- msfs/winwing_ursa_PMDG737.lua
+++ msfs/winwing_ursa_PMDG737.lua
@@ -26,3 +26,30 @@ wwursa:CfgRpn(1, '(L:switch_688_73X) 0 == if{ 68801 (>K:ROTOR_BRAKE) }')
 wwursa:CfgRpn(2, '(L:switch_689_73X) 100 == if{ 68901 (>K:ROTOR_BRAKE) }')
 -- ENG R CUTOFF
 wwursa:CfgRpn(3, '(L:switch_689_73X) 0 == if{ 68901 (>K:ROTOR_BRAKE) }')
+
+-- ====vibration
+wwursa:GetVibL('(A:SIM ON GROUND,Bool) (A:GPS GROUND SPEED,Meters per second) * 10 *')
+wwursa:GetVibR('(A:SIM ON GROUND,Bool) (A:GPS GROUND SPEED,Meters per second) * 10 *')
+
+-- ====rudder trim LCD
+-- setLcdText caches text internally; USB write only when text changes
+local dr_trim = iDataRef:New('(A:RUDDER TRIM PCT,Percent)')
+
+function Wwursa_PMDG737_LCD_Loop()
+    wwursa:setLcdText(wwursa:formatTrimText(dr_trim:Get(), false))
+end
+
+-- ====backlight
+-- generic MSFS instrument dimming; SetBkl() is ChangedUpdate throttled
+wwursa:GetBkl('(A:LIGHT POTENTIOMETER:3, Percent)', 255)
+-- GetOverallBkl drives Throttle ch2 (overall brightness master gate) which
+-- mirrors to PAC ch2 (LCD brightness); binary on/off per dimming > 0
+wwursa:GetOverallBkl('(A:LIGHT POTENTIOMETER:3, Percent)', 255)
+
+GlobalFrameLoopManager:add(function()
+    wwursa:SetBkl()
+    wwursa:SetOverallBkl()
+    wwursa:SetVibL()
+    wwursa:SetVibR()
+    Wwursa_PMDG737_LCD_Loop()
+end)
--- msfs/z_WWURSA_GA.lua
+++ msfs/z_WWURSA_GA.lua
@@ -8,7 +8,33 @@ if not wwursa then return end
 -- Do not remove above lines: hardware detection
 
 uluaLog('Wwursa for GA')
---[[
+
+--====vibration
+wwursa:GetVibL('(A:SIM ON GROUND,Bool) (A:GPS GROUND SPEED,Meters per second) * 10 *')
+wwursa:GetVibR('(A:SIM ON GROUND,Bool) (A:GPS GROUND SPEED,Meters per second) * 10 *')
+
+--====rudder trim LCD
+-- setLcdText caches text internally; USB write only when text changes
+local dr_trim = iDataRef:New('(A:RUDDER TRIM PCT,Percent)')
+
+function Wwursa_GA_LCD_Loop()
+    wwursa:setLcdText(wwursa:formatTrimText(dr_trim:Get(), false))
+end
+
+--====backlight
+-- generic MSFS instrument dimming; SetBkl() is ChangedUpdate throttled
+wwursa:GetBkl('(A:LIGHT POTENTIOMETER:3, Percent)', 255)
+-- GetOverallBkl drives Throttle ch2 (overall brightness master gate) which
+-- mirrors to PAC ch2 (LCD brightness); binary on/off per dimming > 0
+wwursa:GetOverallBkl('(A:LIGHT POTENTIOMETER:3, Percent)', 255)
+
+
+
+
+GlobalFrameLoopManager:add(function()
+    wwursa:SetBkl()
+    wwursa:SetOverallBkl()
+    --[[
 wwursa:GetBkl('')
 wwursa:GetOverallBkl('')
 wwursa:GetFault1('')
@@ -20,10 +46,7 @@ wwursa:GetVibR('')
 wwursa:GetBkl('')
 wwursa:GetLcdBkl('')
 ]] --
-
-wwursa:GetVibL('(A:SIM ON GROUND,Bool)')
-wwursa:GetVibR('(A:SIM ON GROUND,Bool)')
-GlobalFrameLoopManager:add(function()
     wwursa:SetVibL()
     wwursa:SetVibR()
+    Wwursa_GA_LCD_Loop()
 end)
