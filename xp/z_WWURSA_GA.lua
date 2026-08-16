61f716c48a02ba2b685b9533be1ab253233bbc69
Wei Shuai <cpuwolf@sina.com>
2026-08-17 06:57:38 +0800

    URSA left and right tire divided

--- xp/z_WWURSA_GA.lua
+++ xp/z_WWURSA_GA.lua
@@ -1,4 +1,3 @@
-
 -- *****************************************************************
 -- created by Wei Shuai <cpuwolf@gmail.com> 2026-08-08_03_55_08UTC
 -- *****************************************************************
@@ -15,18 +14,27 @@ uluaLog('Wwursa for GA')
 -- explicit SendLedCmd with local throttling (no per-frame USB HID traffic)
 local dr_onground = iDataRef:New('sim/flightmodel/failures/onground_any')
 local dr_gs = iDataRef:New('sim/flightmodel/position/groundspeed')
-local vib_last = 0
+local dr_l_tire = iDataRef:New('sim/flightmodel2/gear/tire_vertical_deflection_mtr[1]')
+local dr_r_tire = iDataRef:New('sim/flightmodel2/gear/tire_vertical_deflection_mtr[2]')
+local vib_l_last = 0
+local vib_r_last = 0
 
 function Wwursa_GA_Vib_Loop()
-	local vib = 0
+	local vib_l, vib_r = 0, 0
 	if dr_onground:Get() ~= 0 then
-		vib = math.floor(dr_gs:Get())
-		if vib > 255 then vib = 255 end
+		local gs = dr_gs:Get() * 10
+		vib_l = math.floor(gs * dr_l_tire:Get())
+		vib_r = math.floor(gs * dr_r_tire:Get())
+		if vib_l > 255 then vib_l = 255 end
+		if vib_r > 255 then vib_r = 255 end
 	end
-	if vib ~= vib_last then
-		vib_last = vib
-		wwursa:SendLedCmd(wwursa.LEDS_VIBL, vib)
-		wwursa:SendLedCmd(wwursa.LEDS_VIBR, vib)
+	if vib_l ~= vib_l_last then
+		vib_l_last = vib_l
+		wwursa:SendLedCmd(wwursa.LEDS_VIBL, vib_l)
+	end
+	if vib_r ~= vib_r_last then
+		vib_r_last = vib_r
+		wwursa:SendLedCmd(wwursa.LEDS_VIBR, vib_r)
 	end
 end
 
@@ -56,7 +64,7 @@ wwursa:GetVibL('')
 wwursa:GetVibR('')
 wwursa:GetBkl('')
 wwursa:GetLcdBkl('')
-]]--
+]] --
 
 
 GlobalFrameLoopManager:add(function()
