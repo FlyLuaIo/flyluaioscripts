254c3818b77bb792de02b9620d69c36f27c12453
Wei Shuai <cpuwolf@sina.com>
2026-08-20 09:48:30 +0800

    Update WWPDC3N_Zibo.lua

--- xp/WWPDC3N_Zibo.lua
+++ xp/WWPDC3N_Zibo.lua
@@ -1,6 +1,5 @@
 -- *****************************************************************
 -- WwPdc3n (captain / 3N L) for Zibo B738
--- Ported from WINCTRL zibo-pdc-profile (3N button index column)
 -- *****************************************************************
 
 if ilua_require_zibo() then return end
@@ -16,19 +15,6 @@ local side = 'capt'
 local pilot = 'pilot'
 local cptFo = 'cpt'
 
-function wwpdc3n_zibo_seek(posRef, dnCmd, upCmd, target)
-	local dr = uluaFind(posRef)
-	if not dr then return end
-	local cur = uluaGet(dr)
-	local cmdDn = uluaFind(dnCmd)
-	local cmdUp = uluaFind(upCmd)
-	if cur < target then
-		for _ = cur, target - 1 do uluaCmdOnce(cmdUp) end
-	elseif cur > target then
-		for _ = target, cur - 1 do uluaCmdOnce(cmdDn) end
-	end
-end
-
 -------------------- Input Keys Binding ---------------------
 wwpdc3n:CfgCmd(0, 'laminar/B738/EFIS_control/' .. side .. '/push_button/fpv_press')
 wwpdc3n:CfgCmd(1, 'laminar/B738/EFIS_control/' .. side .. '/push_button/mtrs_press')
@@ -40,12 +26,19 @@ wwpdc3n:CfgCmd(6, 'laminar/B738/EFIS_control/' .. side .. '/push_button/data_pre
 wwpdc3n:CfgCmd(7, 'laminar/B738/EFIS_control/' .. side .. '/push_button/pos_press')
 wwpdc3n:CfgCmd(8, 'laminar/B738/EFIS_control/' .. side .. '/push_button/terr_press')
 
-wwpdc3n:CfgFc(9, 'wwpdc3n_zibo_seek("laminar/B738/EFIS_control/capt/vor1_off_pos","laminar/B738/EFIS_control/capt/vor1_off_dn","laminar/B738/EFIS_control/capt/vor1_off_up",1)')
-wwpdc3n:CfgFc(10, 'wwpdc3n_zibo_seek("laminar/B738/EFIS_control/capt/vor1_off_pos","laminar/B738/EFIS_control/capt/vor1_off_dn","laminar/B738/EFIS_control/capt/vor1_off_up",0)')
-wwpdc3n:CfgFc(11, 'wwpdc3n_zibo_seek("laminar/B738/EFIS_control/capt/vor1_off_pos","laminar/B738/EFIS_control/capt/vor1_off_dn","laminar/B738/EFIS_control/capt/vor1_off_up",-1)')
-wwpdc3n:CfgFc(12, 'wwpdc3n_zibo_seek("laminar/B738/EFIS_control/capt/vor2_off_pos","laminar/B738/EFIS_control/capt/vor2_off_dn","laminar/B738/EFIS_control/capt/vor2_off_up",1)')
-wwpdc3n:CfgFc(13, 'wwpdc3n_zibo_seek("laminar/B738/EFIS_control/capt/vor2_off_pos","laminar/B738/EFIS_control/capt/vor2_off_dn","laminar/B738/EFIS_control/capt/vor2_off_up",0)')
-wwpdc3n:CfgFc(14, 'wwpdc3n_zibo_seek("laminar/B738/EFIS_control/capt/vor2_off_pos","laminar/B738/EFIS_control/capt/vor2_off_dn","laminar/B738/EFIS_control/capt/vor2_off_up",-1)')
+-- VOR1 selector (Buttons 10..12 -> bits 9..11: VOR / OFF / ADF)
+local pswh_vor1 = QmdevPosSwitchInit("laminar/B738/EFIS_control/capt/vor1_off_pos", 1,
+	"laminar/B738/EFIS_control/capt/vor1_off_up", "laminar/B738/EFIS_control/capt/vor1_off_dn", 300)
+wwpdc3n:CfgPSw(9, pswh_vor1, 1)   -- VOR
+wwpdc3n:CfgPSw(10, pswh_vor1, 0)  -- OFF
+wwpdc3n:CfgPSw(11, pswh_vor1, -1) -- ADF
+
+-- VOR2 selector (Buttons 13..15 -> bits 12..14: VOR / OFF / ADF)
+local pswh_vor2 = QmdevPosSwitchInit("laminar/B738/EFIS_control/capt/vor2_off_pos", 1,
+	"laminar/B738/EFIS_control/capt/vor2_off_up", "laminar/B738/EFIS_control/capt/vor2_off_dn", 300)
+wwpdc3n:CfgPSw(12, pswh_vor2, 1)  -- VOR
+wwpdc3n:CfgPSw(13, pswh_vor2, 0)  -- OFF
+wwpdc3n:CfgPSw(14, pswh_vor2, -1) -- ADF
 
 wwpdc3n:CfgCmd(15, 'laminar/B738/EFIS_control/' .. side .. '/push_button/rst_press')
 wwpdc3n:CfgCmd(16, 'laminar/B738/EFIS_control/' .. side .. '/push_button/ctr_press')
@@ -58,11 +51,17 @@ wwpdc3n:CfgCmd(21, 'laminar/B738/' .. pilot .. '/barometer_down')
 wwpdc3n:CfgCmd(22, 'laminar/B738/' .. pilot .. '/barometer_up')
 
 -- mins RADIO/BARO: up/dn inverted in Zibo (WINCTRL note)
-wwpdc3n:CfgFc(23, 'wwpdc3n_zibo_seek("laminar/B738/EFIS_control/cpt/minimums","laminar/B738/EFIS_control/cpt/minimums_up","laminar/B738/EFIS_control/cpt/minimums_dn",0)')
-wwpdc3n:CfgFc(24, 'wwpdc3n_zibo_seek("laminar/B738/EFIS_control/cpt/minimums","laminar/B738/EFIS_control/cpt/minimums_up","laminar/B738/EFIS_control/cpt/minimums_dn",1)')
-wwpdc3n:CfgFc(25, 'wwpdc3n_zibo_seek("laminar/B738/EFIS_control/capt/baro_in_hpa","laminar/B738/EFIS_control/capt/baro_in_hpa_dn","laminar/B738/EFIS_control/capt/baro_in_hpa_up",0)')
-wwpdc3n:CfgFc(26, 'wwpdc3n_zibo_seek("laminar/B738/EFIS_control/capt/baro_in_hpa","laminar/B738/EFIS_control/capt/baro_in_hpa_dn","laminar/B738/EFIS_control/capt/baro_in_hpa_up",1)')
+local pswh_mins = QmdevPosSwitchInit("laminar/B738/EFIS_control/cpt/minimums", 1,
+	"laminar/B738/EFIS_control/cpt/minimums_dn", "laminar/B738/EFIS_control/cpt/minimums_up", 300)
+wwpdc3n:CfgPSw(23, pswh_mins, 0) -- RADIO
+wwpdc3n:CfgPSw(24, pswh_mins, 1) -- BARO
 
+local pswh_baro = QmdevPosSwitchInit("laminar/B738/EFIS_control/capt/baro_in_hpa", 1,
+	"laminar/B738/EFIS_control/capt/baro_in_hpa_up", "laminar/B738/EFIS_control/capt/baro_in_hpa_dn", 300)
+wwpdc3n:CfgPSw(25, pswh_baro, 0) -- IN
+wwpdc3n:CfgPSw(26, pswh_baro, 1) -- HPA
+
+-- MODE rotary (Buttons 28..31 -> bits 27..30: APP / VOR / MAP / PLN)
 wwpdc3n:CfgVal(27, 'laminar/B738/EFIS_control/' .. side .. '/map_mode_pos', 0)
 wwpdc3n:CfgVal(28, 'laminar/B738/EFIS_control/' .. side .. '/map_mode_pos', 1)
 wwpdc3n:CfgVal(29, 'laminar/B738/EFIS_control/' .. side .. '/map_mode_pos', 2)
