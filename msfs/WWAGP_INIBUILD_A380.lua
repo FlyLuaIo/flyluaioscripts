36b0d6fb71f1c6127f4b73fb917cbccc05089648
Wei Shuai <cpuwolf@sina.com>
2026-09-17 09:12:23 +0800

    Update WWAGP_INIBUILD_A380.lua

--- msfs/WWAGP_INIBUILD_A380.lua
+++ msfs/WWAGP_INIBUILD_A380.lua
@@ -14,13 +14,31 @@ uluaLog('WinWing AGP for Inibuild A380')
 --================================ Output key binding
 --
 wwagp:CfgRpn(0, "(L:INI_BRAKE_FAN_ON) ! (>L:INI_BRAKE_FAN_ON)", "(L:INI_BRAKE_FAN_ON) ! (>L:INI_BRAKE_FAN_ON)")
+
 -- autobrake
 wwagp:CfgRpn(2,
-	"(L:INI_AUTOBRAKE_LEVEL, number) 5 != if{ 5 (>L:INI_AUTOBRAKE_LEVEL, number) } els{ 0 (>L:INI_AUTOBRAKE_LEVEL, number) }")
+	"(L:INI_LG_ABRK_LEVEL, number) 2 != if{ 2 (>L:INI_LG_ABRK_LEVEL, number) } els{ 0 (>L:INI_LG_ABRK_LEVEL, number) }")
 wwagp:CfgRpn(3,
-	"(L:INI_AUTOBRAKE_LEVEL, number) 3 != if{ 3 (>L:INI_AUTOBRAKE_LEVEL, number) } els{ 0 (>L:INI_AUTOBRAKE_LEVEL, number) }")
-wwagp:CfgRpn(4,
-	"(L:INI_AUTOBRAKE_LEVEL, number) 4 != if{ 4 (>L:INI_AUTOBRAKE_LEVEL, number) } els{ 0 (>L:INI_AUTOBRAKE_LEVEL, number) }")
+	"(L:INI_LG_ABRK_LEVEL, number) 3 != if{ 3 (>L:INI_LG_ABRK_LEVEL, number) } els{ 0 (>L:INI_LG_ABRK_LEVEL, number) }")
+local drf_brk_pos = iDataRef:New("(L:INI_LG_ABRK_LEVEL, number)")
+function key_brkmax_long_end_func()
+	uluaWriteCmd('1 (>B:AIRLINER_MIP_LG_ABRK_RTO_Set)')
+end
+
+function key_brkmax_long_func()
+	uluaWriteCmd('0 (>L:INI_LG_ABRK_LEVEL, number)')
+	uluasetTimeout('key_brkmax_long_end_func()', 2000)
+end
+
+function key_brkmax_short_func()
+	if drf_brk_pos:Get() == 5 then
+		uluaWriteCmd('0 (>L:INI_LG_ABRK_LEVEL, number)')
+	else
+		uluaWriteCmd('5 (>L:INI_LG_ABRK_LEVEL, number)')
+	end
+end
+
+wwagp:CfgLongFc(4, 1000, key_brkmax_long_func, key_brkmax_short_func)
 
 wwagp:CfgRpn(5, '0 (>L:INI_ANTISKID_SWITCH)')
 wwagp:CfgRpn(6, '1 (>L:INI_ANTISKID_SWITCH)')
@@ -28,9 +46,9 @@ wwagp:CfgRpn(8, '1 (>L:INI_CHR_RESET_COMMAND)')
 wwagp:CfgRpn(11, '1 (>L:INI_CHR_START_COMMAND)')
 wwagp:CfgRpn(14, '1 (>L:INI_CLOCK_DATE_BUTTON)', '0 (>L:INI_CLOCK_DATE_BUTTON)')
 
-wwagp:CfgRpn(16, '2 (>L:INI_CLOCK_GPS_STATE)')
+wwagp:CfgRpn(16, '0 (>L:INI_CLOCK_GPS_STATE)')
 wwagp:CfgRpn(17, '1 (>L:INI_CLOCK_GPS_STATE)')
-wwagp:CfgRpn(18, '0 (>L:INI_CLOCK_GPS_STATE)')
+wwagp:CfgRpn(18, '2 (>L:INI_CLOCK_GPS_STATE)')
 
 wwagp:CfgRpn(19, '0 (>L:INI_CLOCK_RUN_STATE)')
 wwagp:CfgRpn(20, '1 (>L:INI_CLOCK_RUN_STATE)')
@@ -42,7 +60,7 @@ wwagp:CfgRpn(23, '(>K:GEAR_UP)', '(>K:GEAR_DOWN)')
 
 
 --====backlight
-wwagp:GetBkl("(L:INI_CKPT_LT_INTEG)", 2.4)            -- 0~100
+wwagp:GetBkl("(L:INI_CKPT_LT_INTEG)", 2.4)          -- 0~100
 wwagp:GetDigiBkl("(L:INI_battery_on, number)", 200) -- 0~1
 wwagp:GetLedBkl("(L:INI_battery_on, number)", 200)  -- 0~1
 --================================ Input LED/LCD ===
@@ -56,10 +74,11 @@ wwagp:GetLockR("(L:INI_GEAR2_POSITION) 50 ==")
 wwagp:GetBrakeOn('(L:INI_BRAKE_FAN_ON)')
 wwagp:GetLowD('(L:INI_AUTOBRAKE_LOW_DECEL)')
 wwagp:GetMedD('(L:INI_AUTOBRAKE_MED_DECEL)')
-wwagp:GetMaxD('(L:INI_AUTOBRAKE_HI_DECEL)')
-wwagp:GetLow('(L:INI_AUTOBRAKE_LEVEL) 5 ==')
-wwagp:GetMed('(L:INI_AUTOBRAKE_LEVEL) 3 ==')
-wwagp:GetMax('(L:INI_AUTOBRAKE_LEVEL) 4 ==')
+wwagp:GetMaxD('(L:INI_AUTOBRAKE_LEVEL) 4 == (L:INI_AUTOBRAKE_ARMED) and')
+wwagp:GetLow('(L:INI_LG_ABRK_LEVEL) 2 ==')
+wwagp:GetMed('(L:INI_LG_ABRK_LEVEL) 3 ==')
+wwagp:GetMax(
+	'(L:INI_AUTOBRAKE_LEVEL) 4 == (L:INI_AUTOBRAKE_ARMED) and (L:INI_LG_ABRK_LEVEL) 5 == or')
 wwagp:GetTerr('(L:INI_TERR_ON_CAPT)')
 wwagp:GetLever('(L:INI_GEAR_DOWN_ARROW_LIGHT)')
 
@@ -67,7 +86,8 @@ wwagp:GetLever('(L:INI_GEAR_DOWN_ARROW_LIGHT)')
 
 --====LCD
 local dr_running_chrono = iDataRef:New('(L:INI_CHRONO_STARTED, number)')
-local dr_chrono = iDataRef:New('(L:INI_CHRONO_UPPER, number)')
+local dr_chrono_sec = iDataRef:New('(L:INI_CHRONO_SECONDS, number)')
+local dr_chrono_min = iDataRef:New('(L:INI_CHRONO_MINUTES, number)')
 
 local dr_utc_year = iDataRef:New('(E:ZULU YEAR, number)')
 local dr_utc_mon = iDataRef:New('(E:ZULU MONTH OF YEAR, number)')
@@ -76,7 +96,8 @@ local dr_utc_day = iDataRef:New('(E:ZULU DAY OF MONTH, number)')
 local dr_utc_sec = iDataRef:New('(E:ZULU TIME, second)')
 
 local dr_running_et = iDataRef:New('(L:INI_ET_STARTED, number)')
-local dr_et_sec = iDataRef:New('(E:SIMULATION TIME, second)')
+local dr_et_min = iDataRef:New('(L:INI_ET_MINUTE, number)')
+local dr_et_hr = iDataRef:New('(L:INI_ET_HOUR, number)')
 
 local dr_utc_is_date = iDataRef:New('(L:INI_CLOCK_DATE_BUTTON)')
 
@@ -85,12 +106,14 @@ local gUtc = ""
 local elapsed_time = ""
 function Wwagp_GA_LCD_Loop()
 	--Chrone
-	if dr_chrono:ChangedUpdate() or dr_running_chrono:ChangedUpdate() then
-		local chr = dr_chrono:GetOld()
+	if dr_chrono_sec:ChangedUpdate() or dr_running_chrono:ChangedUpdate() or dr_chrono_min:ChangedUpdate() then
+		local chr = dr_chrono_sec:GetOld()
+		local chr_min = dr_chrono_min:GetOld()
 		local chr_is_run = dr_running_chrono:GetOld()
 		if chr_is_run == 0 then
 			gChrono = "     "
 		else
+			chr = chr + chr_min * 60
 			gChrono = wwagp:formatChronoStr(chr)
 		end
 	end
@@ -119,10 +142,9 @@ function Wwagp_GA_LCD_Loop()
 	end
 
 	-- ET
-	if dr_et_sec:ChangedUpdate() or dr_running_et:ChangedUpdate() then
-		local totalSeconds = math.floor(dr_et_sec:GetOld())
-		local h = math.floor(totalSeconds / 3600)
-		local m = math.floor((totalSeconds % 3600) / 60)
+	if dr_et_min:ChangedUpdate() or dr_et_hr:ChangedUpdate() or dr_running_et:ChangedUpdate() then
+		local m = math.floor(dr_et_min:GetOld())
+		local h = math.floor(dr_et_hr:GetOld())
 		local et_is_run = dr_running_et:GetOld()
 		if et_is_run == 0 then
 			elapsed_time = "     "
